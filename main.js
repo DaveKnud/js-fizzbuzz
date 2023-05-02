e4352698e52fd5e1fa2bd3b78452191f7f59f314
DaveKnud <daveknud@outlook.com>
2023-05-02 15:44:45 +0200

    Creazione di multipli *3 e *5

---
+++ main.js
@@ -0,0 +1,28 @@
+//select new container
+const contenitore = document.getElementById("container");
+console.log(container);
+
+
+//creazione del ciclo e output
+for (let i =1; i <= 100; i++) {
+    if(i % 3 === 0) { //se il numero e' un multiplo di 3
+        console.log("Fizz");
+    }
+
+    else if{(i % 5 === 0) { //se il numero e' un multiplo di 5
+        console.log("Buzz");
+
+    }
+    
+    i
+    //creazione nuovo elemento
+    const quadrato = document.createElement('span');
+       
+    //inserimento valore nell'elemento
+    quadrato.append(i);
+
+    //inserimento elemento al contenitore
+    container.append(quadrato);
+}
+
+
