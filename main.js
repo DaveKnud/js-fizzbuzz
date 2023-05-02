e480e512b345f4d8fdcce2b521dcad26d63a30c9
DaveKnud <daveknud@outlook.com>
2023-05-02 17:23:32 +0200

    stampa andata

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ for (let i = 1; i <= 100; i++) {
 
     else{
         console.log(i)
-        contenitore.innerHTML += `<div class="box">${i}</div>`;
+        container.innerHTML += `<div class="box">${i}</div>`;
     }
 
 }
