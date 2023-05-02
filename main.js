d7f8a083bf12b6bb081c301c47c926c71849fcba
DaveKnud <daveknud@outlook.com>
2023-05-02 17:10:12 +0200

    impallato

--- main.js
+++ main.js
@@ -1,40 +1,33 @@
 //select new container
-const contenitore = document.getElementById("container");
+const container = document.getElementById("container");
 console.log(container);
 
 
 
 //creazione del ciclo e output
-for (let i =1; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
     
     if(i % 15 === 0) { //se il numero e' un multiplo di 5
-        document.getElementById("container").innerHTML = "Fizz Buzz";
-        console.log("Fizz Buzz" + i);
+        container.innerHTML += `<div class="box box-15">FizzBuzz</div>`;
+        console.log("Fizz Buzz");
     }
 
     else if(i % 5 === 0) { //se il numero e' un multiplo di 5
-        document.getElementById("container").innerHTML = "Buzz";
-        console.log("Buzz" + i);
+        container.innerHTML += `<div class="box box-5">FizzBuzz</div>`;
+        console.log("Buzz");
     }
-
-
-    else if(i % 3 === 0) { //se il numero e' un multiplo di 3
-        document.getElementById("container").innerHTML = "Fizz";
-        console.log("Fizz" + i);
-    }
-
- 
-
-
     
-    //creazione nuovo elemento
-    const quadrato = document.createElement('span');
-       
-    //inserimento valore nell'elemento
-    quadrato.append(i);
+    else if(i % 3 === 0) { //se il numero e' un multiplo di 3
+        container.innerHTML += `<div class="box box-3">FizzBuzz</div>`;
+        console.log("Fizz");
+    }
 
-    //inserimento elemento al contenitore
-    container.append(quadrato);
+    else{
+        console.log(i)
+        contenitore.innerHTML += `<div class="box">${i}</div>`;
+    }
+
 }
+
 
 
