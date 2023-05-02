21050cd161a02e7a731a2b1b8b279f300cc02eb1
DaveKnud <daveknud@outlook.com>
2023-05-02 16:10:22 +0200

    Correzione ordine multipli (dal maggiore al minore e non viceversa)

--- main.js
+++ main.js
@@ -3,15 +3,29 @@ const contenitore = document.getElementById("container");
 console.log(container);
 
 
+
 //creazione del ciclo e output
 for (let i =1; i <= 100; i++) {
-    if(i % 3 === 0) { //se il numero e' un multiplo di 3
-        console.log("Fizz");
+    
+    if(i % 15 === 0) { //se il numero e' un multiplo di 5
+        document.getElementById("container").innerHTML = "Fizz Buzz";
+        console.log("Fizz Buzz" + i);
     }
 
     else if(i % 5 === 0) { //se il numero e' un multiplo di 5
-        console.log("Buzz");
+        document.getElementById("container").innerHTML = "Buzz";
+        console.log("Buzz" + i);
     }
+
+
+    else if(i % 3 === 0) { //se il numero e' un multiplo di 3
+        document.getElementById("container").innerHTML = "Fizz";
+        console.log("Fizz" + i);
+    }
+
+ 
+
+
     
     //creazione nuovo elemento
     const quadrato = document.createElement('span');
